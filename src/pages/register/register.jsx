6d4285b086275bb5e12db71b8800ebc976ee8fb0
Joao Vitor Rodrigues <joao.rodrigues@teracorporation.com.br>
2026-08-20 18:52:57 -0300

    Utilizando registro com useState

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,6 +1,29 @@
 import { Link } from "react-router-dom";
+import { useState } from "react"
 
 function Register() {
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+  const [confirmarSenha, setConfirmarSenha] = useState("")
+
+  function handleSubmit (e) {
+    e.preventDefault();
+
+    if (senha !== confirmarSenha){
+      alert("As senhas não são iguais")
+      return
+    }
+    const user = {
+      username: username,
+      email: email,
+      senha: senha,
+      senhaComparar: confirmarSenha,
+    }
+
+    console.log(usuario)
+  }
+
   return (
     <div>
       <h1>Cadastro</h1>
@@ -8,22 +31,22 @@ function Register() {
       <form>
         <div>
             <label htmlFor="nome">Nome:</label>
-            <input type="text" id="nome" placeholder="Digite o nome"/>
+            <input type="text" id="nome" placeholder="Digite o nome" value={username} onChange={(e) => setUsername(e.target.value)}/>
         </div>
 
         <div>
             <label htmlFor="email">E-mail:</label>
-            <input type="email" id="email" placeholder="Digite o e-mail"/>
+            <input type="email" id="email" placeholder="Digite o e-mail" value={email} onChange={(e) => setEmail(e.target.value)}/>
         </div>
 
         <div>
             <label htmlFor="senha">Senha:</label>
-            <input type="password" id="senha" placeholder="Digite a senha"/>
+            <input type="password" id="senha" placeholder="Digite a senha" value={senha} onChange={(e) => setSenha(e.target.value)}/>
         </div>
 
         <div>
             <label htmlFor="confirmarSenha">Confirmar Senha:</label>
-            <input type="password" id="confirmarSenha" placeholder="Confirme a senha"/>
+            <input type="password" id="confirmarSenha" placeholder="Confirme a senha" value={confirmarSenha} onChange={(e) => setConfirmarSenha(e.target.value)}/>
         </div>
 
         <button type="submit">Cadastrar</button>
